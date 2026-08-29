039d58185f589f82a24bfe74b682d3355b1c50fe
sagekoala <brandondukescodes@gmail.com>
2026-08-29 17:15:08 +0200

    Add: contains-duplicate-ii - submission-12

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-12.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        std::unordered_map<int, int> hashMap;
+        for (int i = 0; i < nums.size(); i++) {
+            if (hashMap.count(nums[i]) && abs(i - hashMap[nums[i]] <= k)) {
+                return true;
+            }
+            hashMap[nums[i]] = i;
+        }
+        return false;
+    }
+};
