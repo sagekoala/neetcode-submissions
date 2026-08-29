e72cddbb7d9704f9ee08e364df97a7d7ff6c8ea8
sagekoala <brandondukescodes@gmail.com>
2026-08-29 17:05:08 +0200

    Add: duplicate-integer - submission-15

---
+++ Data Structures & Algorithms/duplicate-integer/submission-15.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        std::unordered_set<int> hashSet;
+        for (int num : nums) {
+            if (hashSet.count(num)) return true;
+            hashSet.insert(num);
+        }
+        return false;
+    }
+}; 
