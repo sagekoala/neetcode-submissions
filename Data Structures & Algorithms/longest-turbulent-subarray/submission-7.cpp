de5d9d79090ed75300f62a866f3878547cc8f0b3
sagekoala <brandondukescodes@gmail.com>
2026-09-06 13:00:48 +0200

    Add: longest-turbulent-subarray - submission-7

---
+++ Data Structures & Algorithms/longest-turbulent-subarray/submission-7.cpp
@@ -0,0 +1,18 @@
+class Solution {
+   public:
+    int maxTurbulenceSize(vector<int>& arr) {
+        int L = 0, longest = 1;
+        for (int R = L + 1; R < arr.size(); R++) {
+            if (arr[R] == arr[R - 1]) {
+                L = R;
+            } else if (R >= 2) {
+                if (((arr[R] > arr[R - 1]) && (arr[R - 1] > arr[R - 2])) ||
+                    ((arr[R] < arr[R - 1]) && (arr[R - 1] < arr[R - 2]))) {
+                    L = R - 1;
+                }
+            }
+            longest = max(longest, R - L + 1);
+        }
+        return longest;
+    }
+};
