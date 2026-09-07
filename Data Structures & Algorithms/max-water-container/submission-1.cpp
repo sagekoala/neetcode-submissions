35d035aa36fb12286f0b87081f3601494cf48b80
sagekoala <brandondukescodes@gmail.com>
2026-09-07 15:42:15 +0200

    Add: max-water-container - submission-1

---
+++ Data Structures & Algorithms/max-water-container/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxArea(vector<int>& heights) {
+        int L = 0, R = heights.size() - 1;
+        int maxWater = 0; 
+        while (L < R) {
+            // Update max water
+            int h = min(heights[L], heights[R]);
+            int w = R - L;
+            if (h * w > maxWater) maxWater = h * w;
+
+            // Update pointers, increment/decrement the ptr to shortest bar
+            if (heights[R] < heights[L]) R--;
+            else L++;
+        }
+        return maxWater;
+    }
+};
